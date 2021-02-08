c77b31f3071489cbdfc33ff3d038e37fab9c1bc5
UladzimirSerada <medavikiby@gmail.com>
2021-02-08 16:19:22 +0300

    )(z13)

---
+++ Zadanie13.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/*Задание 13
+Создать последовательность случайных чисел, найти и вывести наибольшее
+из них.*/
+        public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] array = new int[rand.nextInt(20)];
+       // int array = new int;
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(20) + 1;
+            if (array[i] > max) max = array[i];
+        }
+        System.out.println("Случайные числа: " + Arrays.toString(array));
+        System.out.println("Максимальное число: " + max);
+    }
+        }
